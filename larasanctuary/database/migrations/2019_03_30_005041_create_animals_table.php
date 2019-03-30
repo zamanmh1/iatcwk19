646ff7e4ec2cd89b78ff651b1cfac3b84e23f8ba
Hamza Zaman <hamza.zaman@outlook.com>
2019-03-30 01:39:21 +0000

    Initialised Animal database

---
+++ larasanctuary/database/migrations/2019_03_30_005041_create_animals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnimalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 30);
+            $table->year('birth_year');
+            $table->mediumText('description')->nullable();
+            $table->string('image', 256)->nullable();
+            $table->boolean('is_available')->default(1); // is animal available for adoption?
+            $table->enum('type_of_pet',['cat','dog','bird','rabbit', 'horse','ferret',
+            'fish', 'rat/mice', 'amphibian','reptile'])->default('dog');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+}
