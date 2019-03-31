6788a3773028dbbac14eafeea400ba306ae7116c
Hamza Zaman <hamza.zaman@outlook.com>
2019-03-31 02:05:30 +0100

    Added a page controller and some refactoring

---
+++ larasanctuary/resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'About')
+
+@section('content')
+  <h1>This is the about page</h1>
+@endsection
--- larasanctuary/routes/web.php
+++ larasanctuary/routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@index');
+
+Route::get('/about', 'PageController@about');
 
 Route::get('list', 'AnimalController@list');
 
