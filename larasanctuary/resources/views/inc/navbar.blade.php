6c153e0bdee76c5f03f8a1bbc986a0db96589816
Hamza Zaman <hamza.zaman@outlook.com>
2019-04-28 02:39:06 +0100

    Updated navbar hyperlinks

--- larasanctuary/resources/views/inc/navbar.blade.php
+++ larasanctuary/resources/views/inc/navbar.blade.php
@@ -1,78 +1,78 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            Aston Animal Sanctuary
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+  <div class="container">
+    <a class="navbar-brand" href="{{ url('/') }}">
+      Aston Animal Sanctuary
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-              <li class="nav item">
-                  <a class="nav-link" href="/about">About</a>
-                  <a class="nav-link" href="/contact">Contact</a>
-              </li>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left Side Of Navbar -->
+      <ul class="navbar-nav mr-auto">
+        <li class="nav item">
+          <a class="nav-link" href="{{url('about')}}">About</a>
+          <a class="nav-link" href="{{url('contact')}}">Contact</a>
+        </li>
 
-            </ul>
-
-
-
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                  <li class="nav-item">
-                    <a class="nav-link" href = "{{url('animals')}}">List pets</a>
-                  </li>
-
-                  @if (Gate::allows('user'))
-                    <li class="nav-item">
-                      <a class="nav-link" href = "{{url('animals/create')}}">Create pet</a>
-                    </li>
-
-                  @endif
-
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-
-                        </a>
+      </ul>
 
 
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="/dashboard">Dashboard</a>
+      <!-- Right Side Of Navbar -->
+      <ul class="navbar-nav ml-auto">
+        <!-- Authentication Links -->
+        @guest
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+          </li>
+          @if (Route::has('register'))
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+          @endif
+        @else
+          <li class="nav-item">
+            <a class="nav-link" href = "{{url('animals')}}">List pets</a>
+          </li>
 
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
+          @if (Gate::allows('user'))
+            <li class="nav-item">
+              <a class="nav-link" href = "{{url('animals/create')}}">Create pet</a>
+            </li>
+
+          @endif
+
+          <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+              {{ Auth::user()->name }} <span class="caret"></span>
+
+            </a>
 
 
+
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="{{route('dashboard')}}">Dashboard</a>
+
+              <a class="dropdown-item" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">
+              {{ __('Logout') }}
+            </a>
 
 
 
 
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-        </div>
-    </div>
+
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+            </form>
+          </div>
+        </li>
+      @endguest
+    </ul>
+  </div>
+</div>
 </nav>
